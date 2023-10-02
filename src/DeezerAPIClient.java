2f008a06339d918813357d372b3619593c5f37b5
Cole Hearld <hearldcole@gmail.com>
2023-10-02 15:04:36 -0400

    Add OAuth stuff

--- src/DeezerAPIClient.java
+++ src/DeezerAPIClient.java
@@ -1,46 +1,105 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class DeezerAPIClient {
+    public static void main(String[] args) {
+        String appId = "YOUR_APP_ID";
+        String appSecret = "YOUR_APP_SECRET";
+        String myUrl = "YOUR_CALLBACK_URL";
 
-    //TODO: Add OAuth to successfully access deezer API
-    public void APIConnect(String app_id, String app_secret, String my_url) {
-        String apiUrl = "https://api.deezer.com/search?q=artist:\"Ed Sheeran\"";
+        // Start a session (not required in Java, as sessions are typically managed on the server-side)
+        String state = UUID.randomUUID().toString(); // CSRF protection
 
-        try {
-            URL url = new URL(apiUrl);
+        String code = ""; // The code you receive from the Deezer OAuth callback should be set here.
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (code.isEmpty()) {
+            // Redirect to Deezer's OAuth dialog
+            String dialogUrl = "https://connect.deezer.com/oauth/auth.php?app_id=" + appId
+                    + "&redirect_uri=" + myUrl + "&perms=email,offline_access" + "&state=" + state;
 
-            connection.setRequestMethod("GET");
+            System.out.println("Redirecting to: " + dialogUrl);
+            // Implement redirection logic as needed in your application
+        }
 
-            int responseCode = connection.getResponseCode();
+        if (state.equals("received_state_from_deezer")) { // Replace with the actual received state
+            // Exchange the code for an access token
+            String tokenUrl = "https://connect.deezer.com/oauth/access_token.php?app_id="
+                    + appId + "&secret=" + appSecret + "&code=" + code;
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Read and print the response content
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
+            try {
+                String response = sendGET(tokenUrl);
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
+                // Parse the response to get the access token
+                Map<String, String> params = parseQueryString(response);
 
-                in.close();
+                String accessToken = params.get("access_token");
 
-                // Print the response content
-                System.out.println(response.toString());
-            } else {
-                System.err.println("HTTP Request Failed with status code: " + responseCode);
+                // Use the access token to make API requests
+                String apiUrl = "https://api.deezer.com/user/me?access_token=" + accessToken;
+                String apiResponse = sendGET(apiUrl);
+
+                // Handle the API response (e.g., parse JSON)
+                System.out.println("API Response: " + apiResponse);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error: " + e.getMessage());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("The state does not match. You may be a victim of CSRF.");
         }
     }
+
+    // Helper method to send a GET request and receive a response
+    private static String sendGET(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+            return response.toString();
+        } else {
+            throw new Exception("HTTP GET request failed with response code: " + responseCode);
+        }
+    }
+
+    // Helper method to parse query string parameters
+    private static Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> params = new HashMap<>();
+
+        String[] keyValuePairs = queryString.split("&");
+
+        for (String pair : keyValuePairs) {
+            String[] parts = pair.split("=");
+            if (parts.length == 2) {
+                String key = parts[0];
+                String value = parts[1];
+                params.put(key, value);
+            }
+        }
+
+        return params;
+    }
 }
+
 
 
 
