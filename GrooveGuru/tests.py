a9d1496c7503bba88627827a235e368a2153381c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 11:16:16 -0500

    add many features

---
+++ GrooveGuru/tests.py
@@ -0,0 +1,69 @@
+import unittest
+from unittest.mock import Mock, patch
+import pandas as pd
+import numpy as np
+import main
+import json
+
+
+class TestSpotifyFunctions(unittest.TestCase):
+    @patch('spotipy.Spotify')
+    def test_get_recent_tracks(self, mock_spotify):
+        # Mock the Spotify instance
+        sp_instance = mock_spotify.return_value
+        sp_instance.current_user_recently_played.return_value = {'items': [{'track': {'id': '123'}}, {'track': {'id': '456'}}], 'next': None}
+
+        # Call the function to test
+        tracks, dates = main.get_recent_tracks(sp_instance, 2)
+
+        # Perform assertions
+        self.assertEqual(tracks, ['123', '456'])
+        self.assertTrue(all(isinstance(date, str) for date in dates))
+
+    @patch('spotipy.Spotify')
+    def test_get_tracks(self, mock_spotify):
+        # Mock the Spotify instance
+        sp_instance = mock_spotify.return_value
+        sp_instance.track.return_value = {'name': 'Test Track'}
+
+        # Call the function to test
+        track_info = main.get_tracks(sp_instance, '123')
+
+        # Perform assertions
+        self.assertEqual(track_info['name'], 'Test Track')
+
+    def test_get_tracks_id(self):
+        # Test with sample data
+        tracks = [{'track': {'id': '123'}}, {'track': {'id': '456'}}]
+        track_ids = main.get_tracks_id(tracks)
+
+        # Perform assertions
+        self.assertEqual(track_ids, ['123', '456'])
+
+    def test_get_date_played(self):
+        # Test with sample data
+        tracks = [{'played_at': '2022-01-01'}, {'played_at': '2022-01-02'}]
+        dates_played = main.get_date_played(tracks)
+
+        # Perform assertions
+        self.assertEqual(dates_played, ['2022-01-01', '2022-01-02'])
+
+    @patch('spotipy.Spotify')
+    def test_get_audio_features(self, mock_spotify):
+        # Mock the Spotify instance
+        sp_instance = mock_spotify.return_value
+        sp_instance.audio_features.return_value = [{'acousticness': 0.5, 'danceability': 0.7}]
+
+        # Call the function to test
+        audio_features = main.get_audio_features(sp_instance, ['123'])
+
+        # Perform assertions
+        self.assertEqual(audio_features, [{'acousticness': 0.5, 'danceability': 0.7}])
+
+
+    # TODO: Write test for get_recommendations
+
+
+if __name__ == '__main__':
+    unittest.main()
+
