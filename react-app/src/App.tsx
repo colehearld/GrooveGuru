0cc88490051352ebb9affe81cae4a45eaa88273c
Sroftling <sroftzgames@gmail.com>
2023-11-20 18:02:34 -0500

    changed routing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,19 +3,15 @@ import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import UserProfile from "./UserProfile";
-import LoginPage from "./LoginPage";
 import { LikesDislikesProvider } from "./LikesDislikesContext";
-import "./PageTransition.css"; // Import the CSS file
 
 function App() {
   return (
     <Container className="my-6">
       <LikesDislikesProvider>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/user" element={<UserProfile />} />
-          <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </LikesDislikesProvider>
